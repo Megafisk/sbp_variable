c30684f323926324d5aad6659401f56fe3b36aed
Herman Bergkvist <herman.bergkvist@gmail.com>
2023-03-24 23:22:22 +0100

    fixed directions of e and d ops, working outflow to the east

--- konst_b.py
+++ konst_b.py
@@ -18,7 +18,7 @@ m = 101
 N = m * m
 xvec, h = np.linspace(0, 1, m, retstep=True)
 yvec = np.linspace(0, 1, m)
-X, Y = np.meshgrid(xvec, yvec, indexing='ij')
+X, Y = np.meshgrid(xvec, yvec)
 x = X.reshape((N, 1))
 y = Y.reshape((N, 1))
 y_left = yvec.reshape((m, 1))
@@ -32,8 +32,8 @@ B = np.ones(N) * b  # constant wave speed
 # initial data
 # v = np.zeros((N, 1))
 sigma = 0.05
-x0 = 0.5
-y0 = 0.5
+x0 = 0.4
+y0 = 0.6
 v = np.exp(-(x - x0) ** 2 / sigma ** 2 - (y - y0) ** 2 / sigma ** 2)
 v_t = np.zeros((N, 1))
 u = np.vstack((v, v_t))
@@ -59,12 +59,12 @@ H, HI, D1, D2, e_l, e_r, d1_l, d1_r = op
 HH, HHI, (D2x, D2y), (eW, eE, eS, eN), (d1_W, d1_E, d1_S, d1_N) = ops.convert_1d_2d(op, m)
 
 # D = a * (D2x + D2y) - HHI @ (eW @ d1_W.T + eE @ d1_E.T + eN @ d1_N.T + eS @ d1_S.T)
-# C = - HHI @ eE @ eE.T
+C = - HHI @ eE @ H @ eE.T
 # G = - HHI @ eW
-D = D2x + D2y - HHI @ (-eW @ H @ d1_W.T + eE @ H @ d1_E.T + eN @ H @ d1_N.T - eS @ H @ d1_S.T)
+D = (D2x + D2y) - HHI @ (-eW @ H @ d1_W.T + eE @ H @ d1_E.T + eN @ H @ d1_N.T - eS @ H @ d1_S.T)
 
 
-DD = spsp.bmat(((None, spsp.eye(N)), (D, None)))
+DD = spsp.bmat(((None, spsp.eye(N)), (D, C)))
 zeros_N = np.zeros((N, 1))
 
 
@@ -77,7 +77,7 @@ fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 5))
 plt.xlabel("x")
 plt.ylabel("y")
 # mesh = ax.pcolormesh(X, Y, v.reshape((m, m)), shading="nearest", vmin=zlow, vmax=zhigh)
-mesh = ax.imshow(v.reshape(m, m), vmin=zlow, vmax=zhigh)
+mesh = ax.imshow(v.reshape(m, m), vmin=zlow, vmax=zhigh, origin='lower')
 title = plt.title("Phi at t = " + str(0))
 fig.colorbar(mesh, ax=ax)
 fig.tight_layout()
