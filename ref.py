733d8d5e1b9d66b5ac0bfdc4e375b53485e75827
Herman Bergkvist <herman.bergkvist@gmail.com>
2023-04-04 02:10:52 +0200

    Fix flipped x and y directions, add interp function

--- ref.py
+++ ref.py
@@ -15,9 +15,20 @@ def load_reference(filename):
 
     v = np.zeros((m, m))
     for (i, (xi, yi)) in enumerate(ind):
-        v[yi, xi] = v_ref[i]
+        v[xi, yi] = v_ref[i]
+    v = v.reshape((m * m, 1))
 
     params = {key: float(ref[key]) for key in ('a_center', 'b_center', 'freq', 'amp', 't')}
     params['m'] = m
     params['mb'] = mb
     return v, params
+
+
+def interpolate_ref(ref_fn, x, y):
+    v, params = load_reference(ref_fn)
+    m_ref = params['m']
+    ref_2d = v.reshape((m_ref, m_ref))
+    x_ind = np.rint(x * (m_ref - 1)).astype('int64')
+    y_ind = np.rint(y * (m_ref - 1)).astype('int64')
+    ref_interp = ref_2d[x_ind, y_ind]
+    return ref_interp
