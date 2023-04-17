c306dacee2efa2ee8ff8ff985370ebdfb1c2f4c8
Herman Bergkvist <herman.bergkvist@gmail.com>
2023-04-17 16:04:02 +0200

    Add uneven interpolation, and error calculation

--- ref.py
+++ ref.py
@@ -1,5 +1,8 @@
 import scipy.io as spio
 import numpy as np
+import numpy.linalg as nplg
+from scipy.interpolate import interpn
+
 from var_b import Grid
 
 
@@ -26,11 +29,30 @@ def load_reference(filename, folder='../ref_sols/'):
     return v_list, params
 
 
-def interpolate_ref(vl, params, grid: Grid):
-    m_ref = params['m']
-    N_ref = vl.shape[0]
-    ind = np.arange(N_ref).reshape((m_ref, m_ref))
-    x_ind = np.rint(grid.x * (m_ref - 1)).astype('int64')
-    y_ind = np.rint(grid.y * (m_ref - 1)).astype('int64')
-    rows = ind[x_ind, y_ind].reshape((grid.N,))
-    return vl[rows, :]
+def interpolate(vl, g_ref: Grid, g: Grid):
+    """Interpolates vl on grid g_ref onto grid g"""
+    if (g_ref.m - 1) % (g.m - 1) == 0:
+        # if you can pick every x points from g_ref, just pick the corresponding points
+        m_ref = g_ref.m
+        N_ref = g_ref.N
+        ind = np.arange(N_ref).reshape((m_ref, m_ref))
+        x_ind = np.rint(g.x * (m_ref - 1)).astype('int64')
+        y_ind = np.rint(g.y * (m_ref - 1)).astype('int64')
+        rows = ind[x_ind, y_ind].reshape((g.N,))
+        return vl[rows, :]
+    else:
+        p = (g_ref.xvec, g_ref.yvec)
+        return np.hstack([interpn(p, v.reshape(g_ref.shape), g.xy, method='cubic').reshape(g.N, 1) for v in vl.T])
+
+
+def error(v, v_ref, g: Grid):
+    return nplg.norm(v - v_ref, axis=0) * g.h
+
+
+def error_interp(v, v_ref, g, g_ref):
+    v_ref_i = interpolate(v_ref, g_ref, g)  # v_ref interpolated onto g
+    return error(v, v_ref_i, g)
+
+
+def errors_interp(gs, vs, v_ref, g_ref):
+    return np.vstack([error_interp(vs[i], v_ref, gs[i], g_ref) for i in range(len(vs))]).T
